ca49a6fbff146acde58024d23c3903e340c492a6
Richard W. Norton <rwtnorton@gmail.com>
2020-07-06 19:59:36 -0700

    slurp shakespeare file, feed to trie

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 const inputFilename string = `shakespeare-only.txt`
 
-var trie Trie
+var trie *Trie
 
 func init() {
-
+	f, err := os.Open(inputFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	trie = NewTrie()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		tokens := Parse(line)
+		for _, token := range tokens {
+			trie.Insert(token)
+		}
+	}
+	err = scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("imported file %q, parsed %d words", inputFilename, len(trie.Words))
 }
 
 func main() {
